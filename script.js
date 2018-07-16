2b62826210ccf305c1fac8b019f808813892ed30
CISDNET\grosales <rosales.g793036@stu.sanjac.edu>
2018-07-16 16:57:57 -0500

    more features

--- script.js
+++ script.js
@@ -5,10 +5,63 @@
  * Purpose: To solve the issue of how the teachers send requests to fix issues with their chromebooks
  */
 
+// GLOBAL  VARIABLES
+
+//Statuses within the status column
+var COMPLETED = "Completed";
+var IN_PROGRESS = "In progress";
+var OUT_REPAIR = "Out for repair";
+
+//Status to be written inside of the Email Sent column
+var STAT_1 = "COMPLETED";
+var STAT_2 = "IN PROGRESS";
+var STAT_3 = "OUT FOR REPAIR";
+
+//Column numbers
+var ticketCol = 1;
+var emailCol = 2;
+var dateCol = 3;
+var autoEmailCol = 4;
+var campusCol = 5;
+var roomCol = 6;
+var chromeCol = 7;
+var issueCol = 8;
+var statusCol = 9;
+var escalatedCol = 11;
+
+//Getting the variables needed for all functions
+var sheet = SpreadsheetApp.getActiveSheet();
+var startRow = 2; // First row of data to process
+var numRows = 900; // Number of rows to process
+
+var dataRange = sheet.getRange(startRow, autoEmailCol, numRows, 1); //grabbing ranges of values to get from email_sent column
+var data = dataRange.getValues(); //getting values
+
+var emailRange = sheet.getRange(startRow, emailCol, numRows, 1); //grabbing ranges of values to get from email column
+var email = emailRange.getValues(); //getting values
+
+var status = sheet.getRange(startRow, statusCol, numRows, 1); //grabbing ranges of values to get from status column
+var data_status = status.getValues(); //getting values
+
+var chromebookNum = sheet.getRange(startRow, chromeCol, numRows, 1); //grabbing ranges of values to get from chromebook number column
+var chromeNum_data = chromebookNum.getValues(); //getting values
+
+var chromeIssue = sheet.getRange(startRow, issueCol, numRows, 1); //grabbing ranges of values to get from issue column
+var issue_status = chromeIssue.getValues(); //getting values
+
+var date = sheet.getRange(startRow, dateCol, numRows, 1); //grabbing ranges of values to get from date column
+var date_data = date.getValues(); //getting values
+
+var ticketNum = sheet.getRange(startRow, ticketCol, numRows);
+var ticket_data = ticketNum.getValues();
+
+var roomNum = sheet.getRange(startRow, roomCol, numRows);
+var room_data = roomNum.getValues();
+
 function focus() {
   // This function focuses the spreadsheet on tickets that are open rather than simply opening at the top and tells the technician how many open tickets they have
   var sheet = SpreadsheetApp.getActiveSheet();
-  var statusRange = sheet.getRange(2, 8, 900, 1);
+  var statusRange = sheet.getRange(2, statusCol, 900, 1);
   var statusValues = statusRange.getValues();
   var openTickets = 0;
   var focusRow = 0;
@@ -28,43 +81,10 @@ function focus() {
   Browser.msgBox("You have " + openTickets + " open tickets");
 }
 
-// Statuses within the status column
-var COMPLETED = "Completed";
-var IN_PROGRESS = "In progress";
-var OUT_REPAIR = "Out for repair";
-
-//Status to be written inside of the Email Sent column
-var STAT_1 = "COMPLETED";
-var STAT_2 = "IN PROGRESS";
-var STAT_3 = "OUT FOR REPAIR";
-
 /**
  * Sends non-duplicate emails with data from the current spreadsheet.
  */
 function sendemail() {
-  //gets active sheet
-  var sheet = SpreadsheetApp.getActiveSheet();
-  var startRow = 2; // First row of data to process
-  var numRows = 900; // Number of rows to process
-
-  var dataRange = sheet.getRange(startRow, 3, numRows, 1); //grabbing ranges of values to get from email_sent column
-  var data = dataRange.getValues(); //getting values
-
-  var emailRange = sheet.getRange(startRow, 1, numRows, 1); //grabbing ranges of values to get from email column
-  var email = emailRange.getValues(); //getting values
-
-  var status = sheet.getRange(startRow, 8, numRows, 1); //grabbing ranges of values to get from status column
-  var data_status = status.getValues(); //getting values
-
-  var chromebookNum = sheet.getRange(startRow, 6, numRows, 1); //grabbing ranges of values to get from chromebook number column
-  var chromeNum_data = chromebookNum.getValues(); //getting values
-
-  var chromeIssue = sheet.getRange(startRow, 7, numRows, 1); //grabbing ranges of values to get from issue column
-  var issue_status = chromeIssue.getValues(); //getting values
-
-  var date = sheet.getRange(startRow, 2, numRows, 1); //grabbing ranges of values to get from date column
-  var date_data = date.getValues(); //getting values
-
   //logic: if a field is populated and both Column C isn't populated, and Status is Completed, populate corresponding row in column C and send email.
   //stays in for loop untill there is data to be read
   for (var i = 0; email[i] != ""; ++i) {
@@ -95,7 +115,7 @@ function sendemail() {
         replyTo: "darrell.cheney@cvisd.org",
         noReply: true
       });
-      sheet.getRange(startRow + i, 3).setValue(STAT_1);
+      sheet.getRange(startRow + i, autoEmailCol).setValue(STAT_1);
 
       // Make sure the cell is updated right away in case the script is interrupted
       SpreadsheetApp.flush();
@@ -118,7 +138,9 @@ function sendemail() {
         replyTo: "darrell.cheney@cvisd.org",
         noReply: true
       });
-      sheet.getRange(startRow + i, 3).setValue(STAT_2);
+      sheet.getRange(startRow + i, autoEmailCol).setValue(STAT_2);
+
+      SpreadsheetApp.flush();
     }
 
     // If email sent column doesn't contain IN PROGRESS and if the status in the status column is In progress, send email and write IN PROGRESS to Email sent column
@@ -138,7 +160,9 @@ function sendemail() {
         replyTo: "darrell.cheney@cvisd.org",
         noReply: true
       });
-      sheet.getRange(startRow + i, 3).setValue(STAT_3);
+      sheet.getRange(startRow + i, autoEmailCol).setValue(STAT_3);
+
+      SpreadsheetApp.flush();
     }
   }
 }
@@ -146,50 +170,76 @@ function sendemail() {
 var NEW_ISSUE = "New Issue";
 
 function defaultValue() {
-  var sheet = SpreadsheetApp.getActiveSheet();
   var startRow = 1;
-  var numRows = 900;
+  var v = 0;
+
+  var roomNum = sheet.getRange(startRow, roomCol, numRows);
+  var room_data = roomNum.getValues();
 
   //get data from campus column
-  var dataRange = sheet.getRange(startRow, 4, numRows);
+  var dataRange = sheet.getRange(startRow, campusCol, numRows);
   var data = dataRange.getValues();
 
   //get data from status column
-  var statusRange = sheet.getRange(startRow, 8, numRows);
+  var statusRange = sheet.getRange(startRow, statusCol, numRows);
   var status = statusRange.getValues();
 
   //the for loop is initiated until there is something other than '' in the column. If there is '' then it sets the value as New Issue
   for (var i = 0; data[i] != ""; ++i) {
     if (status[i] == "") {
-      sheet.getRange(i + startRow, 8).setValue(NEW_ISSUE);
+      sheet.getRange(i + startRow, statusCol).setValue(NEW_ISSUE);
+      sheet
+        .getRange(i + startRow, ticketCol)
+        .setValue("C" + room_data[i] + ++v);
     }
   }
+
+  //  for (var x = 0; ticket_data[i] ==""; ++x) {
+  //
+  //
+  //
+  //  }
 }
 
 function onColorChange() {
   var ss = SpreadsheetApp.getActiveSheet();
   var startRow = 1; //have to start with one otherwise writing to the cell's don't work as expected
-  var range = ss.getRange(startRow, 10, 900); //getting range
+  var numRows = 900;
+
+  var range = ss.getRange(startRow, escalatedCol, 900); //getting range
   var bgColors = range.getBackgrounds(); //getting cell colors
+
   var escalated_data = range.getValues(); //getting values
   var escalated = "Escalated";
 
   for (var i in bgColors) {
     // if color isn't white and if the value isn't set to escalated, set to escalate and send email
     if (bgColors[i] != "#ffffff" && escalated_data[i] != escalated) {
-      MailApp.sendEmail(
-        "gabriel.rosales@cvisd.org",
-        "cmam",
-        "something isn't white"
-      );
+      var emailAddress = email[i]; // First column
 
-      Browser.msgBox("meets conditions");
-      ss.getRange(++i, 10).setValue(escalated);
+      var dateString = date_data[i].toString();
+
+      dateString = dateString.substring(0, 15);
+      var subject = "Chromebook Repair Needs Escalation";
+      var message =
+        "There is an issue that needs to be addressed.  \n \n *** Ticket Details *** \n Chromebook Number: " +
+        ticket_data[i] +
+        "\n Issue: " +
+        issue_status[i] +
+        " \n Date Submitted: " +
+        dateString +
+        " \n \n Please do NOT reply to this email. If you need to contact your technician, email them at darrell.cheney@cvisd.org";
+      MailApp.sendEmail("gabriel.rosales@cvisd.org", subject, message, {
+        replyTo: "darrell.cheney@cvisd.org",
+        noReply: true
+      });
+
+      ss.getRange(++i, escalatedCol).setValue(escalated);
     }
 
     // if color is white and if the value is set to escalated, it will delete escalated value and set it to ""
     if (bgColors[i] == "#ffffff" && escalated_data[i] == escalated) {
-      ss.getRange(++i, 10).setValue("");
+      ss.getRange(++i, escalatedCol).setValue("");
     }
   }
 }
